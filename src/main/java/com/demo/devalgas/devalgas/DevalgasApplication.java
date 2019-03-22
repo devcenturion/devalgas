286413356cdbe6a4f743b773737580212b53784f
devcenturion <devcenturion@gmail.com>
2019-03-22 18:24:10 +0100

    Initial commit

---
+++ src/main/java/com/demo/devalgas/devalgas/DevalgasApplication.java
@@ -0,0 +1,13 @@
+package com.demo.devalgas.devalgas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevalgasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevalgasApplication.class, args);
+	}
+
+}
